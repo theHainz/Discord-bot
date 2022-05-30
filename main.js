90f4b14e49064f07de16748d3f1c564257b83d5d
theHainz <58784402+theHainz@users.noreply.github.com>
2022-05-30 20:35:05 +0200

    Non family friendly update

--- main.js
+++ main.js
@@ -1,30 +1,48 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
+const date = new Date();
 const TOKEN = '';
 
  client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+  const stringarray = [
+        'The White Liquid?',
+        'HE SAID THE WORD',
+        'SIUUUU',
+        'Milk',
+        'Your Mother',
+        'Gulp',
+        'mmm... soup',
+        'more lean',
+  ];
 
-const stringarray = [
-      'The White Liquid?',
-      'HE SAID THE WORD',
-      'SIUUUU',
-      'Milk',
-      'Your Mother',
-      'Gulp',
-      'mmm... soup',
-      'more lean',
-];
+
+  client.on("message", msg => {
+    if (msg.content === "#Cum") {
+      let randomNumber = Math.floor(Math.random()*stringarray.length);
+      msg.reply(stringarray[randomNumber]);
+    }
+  })
 
 
-client.on("message", msg => {
-  if (msg.content === "Milk") {
-    let randomNumber = Math.floor(Math.random()*stringarray.length);
-    msg.reply(stringarray[randomNumber]);
-  }
-})
+  let day = date.getDay();
+  let month = date.getMonth();
+  let hour = date.getHours();
+  const theday = 25;
+  const themonth = 6;
+
+  client.on("message", msg => {
+    if (msg.content === "#Countdown") {
+      if (day >= 25 && month == 6 || day == 31 && month == 8){
+        msg.reply("No More School!");
+      }
+      else {
+        
+      }
+    }
+  })
 
 client.login(TOKEN)
